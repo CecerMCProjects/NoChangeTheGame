b9b87e8e97c363d48ca912fc5f4720d428b8296b
Cecer <cecer_git@cecer1.com>
2024-01-28 19:52:22 +0000

    Hide fake shield tooltip too

--- src/main/java/com/cecer1/projects/mc/nochangethegame/mixin/swordblocking/HideFakeShieldSlotsMixin.java
+++ src/main/java/com/cecer1/projects/mc/nochangethegame/mixin/swordblocking/HideFakeShieldSlotsMixin.java
@@ -3,6 +3,7 @@ package com.cecer1.projects.mc.nochangethegame.mixin.swordblocking;
 import com.cecer1.projects.mc.nochangethegame.utilities.SwordBlockingHelper;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,9 +18,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class HideFakeShieldSlotsMixin {
 
     @Inject(method = "renderItem(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;IIII)V", at = @At(value = "HEAD"))
-    private void hideFakeShieldSlots(CallbackInfo ci, @Local(argsOnly = true) LocalRef<ItemStack> itemStack) {
+    private void hideFakeShieldItem(CallbackInfo ci, @Local(argsOnly = true) LocalRef<ItemStack> itemStack) {
         if (SwordBlockingHelper.INSTANCE.isFakeShield(itemStack.get())) {
             itemStack.set(ItemStack.EMPTY);
+        }
+    }
+
+    @Inject(method = "renderTooltip(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;II)V", at = @At(value = "HEAD"), cancellable = true)
+    private void hideFakeShieldTooltip(Font font, ItemStack itemStack, int x, int y, CallbackInfo ci) {
+        if (SwordBlockingHelper.INSTANCE.isFakeShield(itemStack)) {
+            ci.cancel();
         }
     }
 }
