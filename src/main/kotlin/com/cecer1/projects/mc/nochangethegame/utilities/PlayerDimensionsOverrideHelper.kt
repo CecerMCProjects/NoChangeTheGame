e1de0d8faef8d30938d9ec6e7e283aba3ca15965
Cecer <cecer_git@cecer1.com>
2024-05-17 02:05:00 +0100

    Update sneak height mixin to use the new structure

--- src/main/kotlin/com/cecer1/projects/mc/nochangethegame/utilities/PlayerDimensionsOverrideHelper.kt
+++ src/main/kotlin/com/cecer1/projects/mc/nochangethegame/utilities/PlayerDimensionsOverrideHelper.kt
@@ -1,7 +1,10 @@
 package com.cecer1.projects.mc.nochangethegame.utilities
 
 import com.cecer1.projects.mc.nochangethegame.NoChangeTheGameMod
+import net.minecraft.world.entity.EntityAttachment
+import net.minecraft.world.entity.EntityAttachments
 import net.minecraft.world.entity.EntityDimensions
+import net.minecraft.world.entity.player.Player
 
 object PlayerDimensionsOverrideHelper {
     private var cachedCrouchDimensionsHeight = Float.NaN
@@ -10,7 +13,12 @@ object PlayerDimensionsOverrideHelper {
     val crouchDimensionsOverride: EntityDimensions
         get() {
             val targetHeight = NoChangeTheGameMod.config.sneakHeight.hitboxHeight
+            val targetEyeHeight = NoChangeTheGameMod.config.sneakHeight.eyeHeight
             if (!cachedCrouchDimensionsHeight.equals(targetHeight)) { // This should be accurate enough for needs... right?
+                cachedCrouchDimensions = EntityDimensions.scalable(0.6f, targetHeight)
+                    .withEyeHeight(targetEyeHeight)
+                    .withAttachments(EntityAttachments.builder()
+                        .attach(EntityAttachment.VEHICLE, Player.DEFAULT_VEHICLE_ATTACHMENT))
                 cachedCrouchDimensions = EntityDimensions.scalable(0.6f, targetHeight)
                 cachedCrouchDimensionsHeight = targetHeight
             }
