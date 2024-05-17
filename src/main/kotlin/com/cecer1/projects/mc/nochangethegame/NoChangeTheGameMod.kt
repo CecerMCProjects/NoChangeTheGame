a48ade9e8ceba86920d3ebc8c1d88554ac1f494f
Cecer <cecer_git@cecer1.com>
2024-05-17 01:52:00 +0100

    Migrate to the newer custom packets API

--- src/main/kotlin/com/cecer1/projects/mc/nochangethegame/NoChangeTheGameMod.kt
+++ src/main/kotlin/com/cecer1/projects/mc/nochangethegame/NoChangeTheGameMod.kt
@@ -4,9 +4,9 @@ import com.cecer1.projects.mc.nochangethegame.config.NCTGConfig
 import com.cecer1.projects.mc.nochangethegame.config.NCTGServerOverrideConfig
 import com.cecer1.projects.mc.nochangethegame.config.NCTGUserConfig
 import com.cecer1.projects.mc.nochangethegame.config.NCTGVanillaConfig
+import com.cecer1.projects.mc.nochangethegame.protocol.ClientboundConfigOverridePacket
+import com.cecer1.projects.mc.nochangethegame.protocol.ServerboundAnnouncePacket
 import com.cecer1.projects.mc.nochangethegame.utilities.ServerBrand
-import com.terraformersmc.modmenu.util.mod.fabric.FabricMod
-import io.netty.buffer.Unpooled
 import me.shedaniel.autoconfig.AutoConfig
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer
 import net.fabricmc.api.ClientModInitializer
@@ -14,10 +14,8 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.fabricmc.fabric.api.networking.v1.PacketSender
-import net.fabricmc.fabric.api.util.NbtType
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 import net.fabricmc.loader.api.FabricLoader
-import net.minecraft.client.Minecraft
-import net.minecraft.client.multiplayer.ClientPacketListener
 import net.minecraft.nbt.ByteArrayTag
 import net.minecraft.nbt.ByteTag
 import net.minecraft.nbt.CompoundTag
@@ -25,16 +23,12 @@ import net.minecraft.nbt.DoubleTag
 import net.minecraft.nbt.FloatTag
 import net.minecraft.nbt.IntArrayTag
 import net.minecraft.nbt.IntTag
-import net.minecraft.nbt.ListTag
 import net.minecraft.nbt.LongArrayTag
 import net.minecraft.nbt.LongTag
 import net.minecraft.nbt.ShortTag
 import net.minecraft.nbt.StringTag
-import net.minecraft.nbt.Tag
 import net.minecraft.network.FriendlyByteBuf
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 import net.minecraft.resources.ResourceLocation
-import java.nio.file.Paths
 
 object NoChangeTheGameMod : ClientModInitializer {
     const val MOD_ID = "nochangethegame"
@@ -68,15 +62,18 @@ object NoChangeTheGameMod : ClientModInitializer {
         
         ClientLoginConnectionEvents.DISCONNECT.register { _, _ -> resetServerState() }
         ClientPlayConnectionEvents.DISCONNECT.register { _, _ -> resetServerState() }
+        
+        PayloadTypeRegistry.playC2S().register(ServerboundAnnouncePacket.TYPE, ServerboundAnnouncePacket.CODEC)
+        PayloadTypeRegistry.playS2C().register(ClientboundConfigOverridePacket.TYPE, ClientboundConfigOverridePacket.CODEC)
+        
+        
         ClientPlayConnectionEvents.JOIN.register { _, out: PacketSender, _ ->
-            val buf = FriendlyByteBuf(Unpooled.buffer()).run {
-                writeUtf(FabricLoader.getInstance().getModContainer(MOD_ID).get().metadata.version.friendlyString)
-            }
-            out.sendPacket(ANNOUNCE_PACKET_ID, buf)
+            val version = FabricLoader.getInstance().getModContainer(MOD_ID).get().metadata.version.friendlyString
+            out.sendPacket(ServerboundAnnouncePacket(version))
         }
         
-        ClientPlayNetworking.registerGlobalReceiver(CONFIG_OVERRIDE_PACKET_ID) { _, _, buf: FriendlyByteBuf, _ ->
-            val nbt: CompoundTag = buf.readNbt() ?: return@registerGlobalReceiver // Ignore invalid payloads
+        ClientPlayNetworking.registerGlobalReceiver(ClientboundConfigOverridePacket.TYPE) { packet, _ ->
+            val nbt: CompoundTag = packet.stuff ?: return@registerGlobalReceiver // Ignore invalid payloads
 
             for (key in nbt.allKeys) {
                 val value = when (val override = nbt.get(key)) {
--- src/main/kotlin/com/cecer1/projects/mc/nochangethegame/protocol/ClientboundConfigOverridePacket.kt
+++ src/main/kotlin/com/cecer1/projects/mc/nochangethegame/protocol/ClientboundConfigOverridePacket.kt
@@ -1,26 +1,27 @@
 package com.cecer1.projects.mc.nochangethegame.protocol
 
 import com.cecer1.projects.mc.nochangethegame.NoChangeTheGameMod
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.FriendlyByteBuf
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload.Type
 import net.minecraft.resources.ResourceLocation
 
-class ClientAnnouncePacket(val version: String) : CustomPacketPayload {
+class ClientboundConfigOverridePacket(val stuff: CompoundTag?) : CustomPacketPayload {
 
     override fun type() = TYPE
     
     companion object {
-        private val PACKET_ID = ResourceLocation(NoChangeTheGameMod.MOD_ID, "announce") 
-        val TYPE: Type<ClientAnnouncePacket> = Type(PACKET_ID)
+        private val PACKET_ID = ResourceLocation(NoChangeTheGameMod.MOD_ID, "config_override") 
+        val TYPE: Type<ClientboundConfigOverridePacket> = Type(PACKET_ID)
 
-        val CODEC: StreamCodec<FriendlyByteBuf, ClientAnnouncePacket> = object : StreamCodec<FriendlyByteBuf, ClientAnnouncePacket> {
-            override fun encode(buf: FriendlyByteBuf, packet: ClientAnnouncePacket) {
-                buf.writeUtf(packet.version)
+        val CODEC: StreamCodec<FriendlyByteBuf, ClientboundConfigOverridePacket> = object : StreamCodec<FriendlyByteBuf, ClientboundConfigOverridePacket> {
+            override fun encode(buf: FriendlyByteBuf, packet: ClientboundConfigOverridePacket) {
+                buf.writeNbt(packet.stuff)
             }
-            override fun decode(buf: FriendlyByteBuf): ClientAnnouncePacket {
-                return ClientAnnouncePacket(buf.readUtf())
+            override fun decode(buf: FriendlyByteBuf): ClientboundConfigOverridePacket {
+                return ClientboundConfigOverridePacket(buf.readNbt())
             }
         }
     }
